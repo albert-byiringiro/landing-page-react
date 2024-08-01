1da3b87ebb129ad38bac3d538009c192c8ba5e9f
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-08-01 19:56:14 +0200

    add service tiles

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,11 +1,33 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCartArrowDown,
+  faCloudDownload,
+  faLanguage,
+  faPlane,
+  faPieChart,
+} from "@fortawesome/free-solid-svg-icons";
 
-console.log(faCoffee);
+import { faWordpress } from "@fortawesome/free-brands-svg-icons";
+
+// IImport dataset
+import data from "../data/servicesData";
+
+const iconMap = {
+  "fa fa-wordpress": faWordpress,
+  "fa fa-cart-arrow-down": faCartArrowDown,
+  "fa fa-cloud-download": faCloudDownload,
+  "fa fa-language": faLanguage,
+  "fa fa-plane": faPlane,
+  "fa fa-pie-chart": faPieChart,
+};
+
 const Services = () => {
+  //   const serviceEls = data.map(service => (
+
+  //   ))
   return (
-    <div className="bg-green-500 p-16">
-      <div className="text-center">
+    <div className="bg-[#6375FF] p-16">
+      <div className="text-center ">
         <h2 className="my-4 text-gray-100 font-bold text-4xl uppercase">
           Our Services
         </h2>
@@ -14,7 +36,20 @@ const Services = () => {
           dapibus leonec.
         </h3>
       </div>
-      <FontAwesomeIcon icon={faCoffee} />
+      <div className=" w-[35%]">
+        <FontAwesomeIcon
+          icon={faWordpress}
+          className="rounded-full px-8 py-8 bg-gradient-to-r from-blue-600 to-blue-500 text-white size-8"
+        />
+        <div className="">
+          <h3 className="text-gray-100">Lorem, ipsum dolor.</h3>
+          <p className="text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et autem
+            asperiores veritatis modi vitae magnam consectetur molestiae labore
+            odit libero!
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
