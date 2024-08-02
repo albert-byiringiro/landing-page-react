6007e5f619eb29135d59ed3a8ab184f08fd36326
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-08-02 04:05:50 +0200

    Add features dataset

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <header className=" px-4 flex items-center py-6 justify-around shadow-md">
       <h1 className="uppercase font-semibold text-gray-900 text-2xl">
-        React Landing Page
+        <a href="#">React Landing Page</a>
       </h1>
       <nav className="">
         <ul className="flex space-x-10 flex-wrap">{navEls}</ul>
---
+++ src/data/featuresData.js
@@ -0,0 +1,37 @@
+import { nanoid } from "nanoid";
+import {
+    faComment,
+    faBullhorn,
+    faPeopleGroup,
+    faMagic,
+    faPieChart,
+} from "@fortawesome/free-solid-svg-icons";
+
+
+export default [
+    {
+        id: nanoid(),
+        image: faComment,
+        service: "Lorem ipsum dolor",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at.",
+    },
+    {
+        id: nanoid(),
+        image: faBullhorn,
+        service: "Consectetur adipiscing",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at.",
+    },
+    {
+        id: nanoid(),
+        image: faPeopleGroup,
+        service: "Consectetur adipiscing",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at.",
+    },
+    {
+        id: nanoid(),
+        image: faMagic,
+        service: "Lorem ipsum dolor",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at.",
+    },
+
+];
